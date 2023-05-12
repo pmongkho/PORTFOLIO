6192964a99a3d5871c988cec41223702525781ef
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-05-11 23:22:39 -0500

    first commit

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import './Projects.css'
-import project1 from './videos/portfolio-app.mp4'
-import project2 from './videos/google-translate.mp4'
-import project3 from './videos/sudoku-solver.mp4'
-import project4 from './videos/tattoo-app.mp4'
-import project5 from './videos/tetris-app.mp4'
-import project6 from './videos/monday-clone-app.mp4'
+import project0 from './videos/sharphouse.mp4'
+import project1 from './videos/tattooconsult.mp4'
+import project2 from './videos/tetris-app.mp4'
+
 import HoverVideoPlayer from 'react-hover-video-player'
 import GitHubIcon from '@mui/icons-material/GitHub'
 
@@ -16,41 +14,16 @@ function Projects() {
 				Projects <GitHubIcon fontSize="50" />
 			</h1>
 			<div className="projects_container">
-				<a
-					href="https://github.com/pmongkho/ReactFirebasePortfolio"
-					target="_blank"
-				>
+				<a href="https://github.com/pmongkho/SHARPHOUSEMEDIA" target="_blank">
 					<div className="project_card">
 						<HoverVideoPlayer
-							videoSrc={project1}
+							style={{ color: "black", width: "350px" }}
+							videoSrc={project0}
 							hoverOverlay={
 								<div className="hover-overlay">
 									<div className="">
-										<h2>Portfolio App</h2>
-
-										<p>REACT, FIREBASE V9</p>
-
-										<p>https://github.com/pmongkho/ReactFirebasePortfolio</p>
-									</div>
-								</div>
-							}
-						/>
-					</div>
-				</a>
-
-				<a
-					href="https://github.com/pmongkho/GOOGLE-TRANSLATE-REACT-AXIOS-EXPRESS"
-					target="_blank"
-				>
-					<div className="project_card">
-						<HoverVideoPlayer
-							style={{ color: "black" }}
-							videoSrc={project2}
-							hoverOverlay={
-								<div className="hover-overlay">
-									<div className="">
-										<h2>Google Translate app</h2>
-										<p>REACT, AXIOS, EXPRESS</p>{" "}
+										<h2>Sharp House Media</h2>
+										<p>REACT, Firebase</p>{" "}
 										<p>
 											https://github.com/pmongkho/GOOGLE-TRANSLATE-REACT-AXIOS-EXPRESS
 										</p>
@@ -60,41 +33,16 @@ function Projects() {
 						/>
 					</div>
 				</a>
-
-				<a href="https://github.com/pmongkho/SUDOKU-SOLVER-API" target="_blank">
+				<a href="#" target="_blank">
 					<div className="project_card">
 						<HoverVideoPlayer
-							style={{ color: "black" }}
-							videoSrc={project3}
+							style={{ color: "black", width: "350px" }}
+							videoSrc={project1}
 							hoverOverlay={
 								<div className="hover-overlay">
 									<div className="">
-										<h2>Sudoku Solver App</h2>
-										<p>JS, AXIOS, EXPRESS, API</p>{" "}
-										<p>https://github.com/pmongkho/SUDOKU-SOLVER-API</p>
-									</div>
-								</div>
-							}
-						/>
-					</div>
-				</a>
-
-				<a
-					href="https://github.com/pmongkho/TATTOO-BOOKING-CONSULT-APP"
-					target="_blank"
-				>
-					<div className="project_card">
-						<HoverVideoPlayer
-							style={{ color: "black" }}
-							videoSrc={project4}
-							hoverOverlay={
-								<div className="hover-overlay">
-									<div className="">
-										<h2>Tattoo Booking App</h2>
-										<p>REACT</p>{" "}
-										<p>
-											https://github.com/pmongkho/TATTOO-BOOKING-CONSULT-APP
-										</p>
+										<h2>Tattoo Consultation</h2>
+										<p>REACT, Django Rest Framework</p> <p></p>
 									</div>
 								</div>
 							}
@@ -105,8 +53,8 @@ function Projects() {
 				<a href="https://github.com/pmongkho/TETRIS-VANILLAJS" target="_blank">
 					<div className="project_card">
 						<HoverVideoPlayer
-							style={{ color: "black" }}
-							videoSrc={project5}
+							style={{ color: "black", width: "350px" }}
+							videoSrc={project2}
 							hoverOverlay={
 								<div className="hover-overlay">
 									<div className="">
@@ -119,28 +67,7 @@ function Projects() {
 						/>
 					</div>
 				</a>
-				<a
-					href="https://github.com/pmongkho/MONDAY-CLONE-REACT-CRUD-ASTRADB"
-					target="_blank"
-				>
-					<div className="project_card">
-						<HoverVideoPlayer
-							style={{ color: "black",}}
-							videoSrc={project6}
-							hoverOverlay={
-								<div className="hover-overlay">
-									<div className="">
-										<h2>Monday Clone</h2>
-										<p>React, AstraDB</p>{" "}
-										<p>
-											https://github.com/pmongkho/MONDAY-CLONE-REACT-CRUD-ASTRADB
-										</p>
-									</div>
-								</div>
-							}
-						/>
-					</div>
-				</a>
+
 				<div className="projects-icon"> </div>
 			</div>
 		</div>
